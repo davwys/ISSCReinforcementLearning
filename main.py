998585d2a340c91a5b7d2b890ced3208666854ab
davwys <david.wyss@hotmail.ch>
2021-03-18 10:09:56 +0100

    Setup packages for baselines

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import gym
-env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+
+env = gym.make('CartPole-v1')
+# Optional: PPO2 requires a vectorized environment to run
+# the env is now wrapped automatically when passing it to the constructor
+# env = DummyVecEnv([lambda: env])
+
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=10000)
+
+obs = env.reset()
+for i in range(1000):
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
     env.render()
-    env.step(env.action_space.sample()) # take a random action
-env.close()
